4500b1848bf543ddeabd751c466086a8d75e2ad1
Samreen Fatima <irfankhans786787789@gmail.com>
2024-09-27 17:12:17 +0500

    completed

---
+++ app.js
@@ -0,0 +1,33 @@
+// var str = "saima";
+// var result = str[0].toUpperCase() + str.slice(1)
+// console.log(result);
+
+// var str = "saima sara";
+// var words =  str.split(" ");
+// var arr = [];
+// for(var i = 0; i < words.length; i++){
+//   var result = words[i][0].toUpperCase() + words[i].slice(1)
+//     arr.push(result)  
+// }
+// console.log(arr.join(" "));
+
+// var str = "saima";
+// console.log(str.indexOf("e"));
+   
+// var str = "saima";
+// console.log(str.charAt(4));
+
+// var num = 1.3;
+// console.log(Math.round(num));
+
+// var num = 3.5
+// console.log(Math.round(num));
+   
+// var num = 4.3;
+// console.log(Math.ceil(num));
+
+
+var randam = Math.random()*7
+var round = Math.round(randam)
+console.log(round);
+
